8365808325cfd8c0a1858e51f4b95ae5aa3951de
Bart Kleijngeld <bartkl@gmail.com>
2023-08-03 10:36:21 +0200

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,91 @@
 use rusqlite::{Connection, Result};
+use std::fs::File;
+use std::io::Write;
+use std::time::Instant;
+
+/* Config. */
+
+const MUSIC_ROOT_DIR: &str = "/media/droppie/libraries/music";
+const BEETS_DB_FILEPATH: &str = "library.db";
+const TAGCACHE_FILEPATH: &str = "tag_cache";
+const MPD_DB_FORMAT: u8 = 2;
+const MPD_VERSION: &str = "0.24";
+const GENRE_DELIMITER: &str = ", ";
+
+/* Types. */
 
 #[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+struct DbItem {
+    path: Vec<u8>,
+    length: f32,
+    artist: String,
+    artist_sort: String,
+    album: String,
+    albumartist: String,
+    albumartist_sort: String,
+    title: String,
+    track: u16,
+    genre: String,
+    year: u16,
+    original_year: u16,
+    disc: u8,
+    composer: String,
+    arranger: String,
+    mb_artistid: String,
+    mb_albumartistid: String,
+    mb_albumid: String,
+    mb_trackid: String,
+    mb_releasetrackid: String,
+    label: String,
 }
 
+//#[derive(Debug)]
+//struct Track {
+//    db_item: DbItem,
+//    genres: Vec<String>,
+//    mtime_item: Option<u32>,
+//    mtime_album: Option<u32>,
+//}
+
 fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+    let conn = Connection::open(&BEETS_DB_FILEPATH)?;
 
-    conn.execute(
-        "CREATE TABLE person (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
-        )",
-        (), // empty list of parameters.
-    )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        (&me.name, &me.data),
-    )?;
+    let mut albums_stmt = conn.prepare(
+        "
+        select
+            items.path,
+            items.length,
+            items.artist,
+            items.artist_sort,
+            items.album,
+            albums.albumartist,
+            albums.albumartist_sort,
+            items.title,
+            items.track,
+            albums.genre,
+            albums.year,
+            albums.original_year,
+            items.disc,
+            items.composer,
+            items.arranger,
+            items.mb_artistid,
+            albums.mb_albumartistid,
+            albums.mb_albumid,
+            items.mb_trackid,
+            items.mb_releasetrackid,
+            albums.label
+        from items
+        left join albums
+        on items.album_id = albums.id
+        order by items.path, items.track
+    ",)?;
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
+    let mut names = Vec::new();
+    let mut rows = albums_stmt.query([])?;
+    while let Some(row) = rows.next()? {
+        names.push(row.get(0)?);
     }
+    println!("{}", names.len());
+    //println!("{:?}", names);
     Ok(())
 }
